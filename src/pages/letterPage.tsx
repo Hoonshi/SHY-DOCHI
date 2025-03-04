190474eb88ae5cac96961ca6144605c35aa04e12
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-03-04 19:56:31 +0900

    refactor: 모바일버전 스타일 수정

--- src/pages/letterPage.tsx
+++ src/pages/letterPage.tsx
@@ -34,7 +34,7 @@ export default function LetterPage({ setIsOpen }: LetterPageProps) {
     <div className="fixed inset-0 w-full max-w-[600px] h-screen bg-[url('/img/도치편지지.jpg')] bg-center bg-no-repeat bg-cover mx-auto ">
       <div className="absolute inset-0 bg-white opacity-50 pointer-events-none"></div>
 
-      <div className="my-6">
+      <div className="my-6 mx-3">
         <div className="flex ">
           <button
             className="text-2xl text-[#3b5bdb]"
@@ -48,16 +48,16 @@ export default function LetterPage({ setIsOpen }: LetterPageProps) {
 
         <form
           onSubmit={handleSubmit(handleLetterSubmit)}
-          className="flex flex-col gap-6 mt-10 mx-2">
+          className="flex flex-col gap-6 mt-10">
           <input
             {...register('nickname')}
-            className="w-full p-2 border border-black/50 rounded-lg focus:bg-blue-100 focus:outline-none"
+            className="w-full p-2 border border-black/30 rounded-lg focus:bg-blue-100 focus:outline-none"
             placeholder="무슨 닉네임으로 보내고 싶나요?"
           />
 
           <textarea
             {...register('content')}
-            className="w-full h-50 p-3 border border-black/50 rounded-lg focus:bg-blue-100 focus:outline-none"
+            className="w-full h-60 p-3 border border-black/30 rounded-lg focus:bg-blue-100 focus:outline-none"
             placeholder="여기에 써주시면 돼요! 만약 이전에 많은 내용이 보내졌다면, 전달이 안될 수 있어요... "
           />
 
