c202f0fb76206c893c4a1065d11afa03cc042fb0
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-03-04 20:41:45 +0900

    feat: alert를 toast로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import 'tailwindcss'
 import Router from './routes/router'
+import { Toaster } from 'react-hot-toast'
 
 export default function App() {
-  return <Router />
+  return (
+    <>
+      <Router />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '10px 14px',
+            backgroundColor: '#4c6ef5',
+            color: '#f8f9fa'
+          }
+        }}
+      />
+    </>
+  )
 }
--- src/pages/letterPage.tsx
+++ src/pages/letterPage.tsx
@@ -2,6 +2,7 @@ import Button from '@/components/Button'
 import axios from 'axios'
 import { createPortal } from 'react-dom'
 import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
 import { IoMdArrowRoundBack } from 'react-icons/io'
 
 type LetterPageProps = {
@@ -18,21 +19,19 @@ export default function LetterPage({ setIsOpen }: LetterPageProps) {
       const response = await axios.post('https://formspree.io/f/mnnjeybd', data)
 
       if (response.status === 200) {
-        alert('편지가 전송되었습니다!')
+        toast.success('전달 됐어요!', { icon: '🥰' })
         reset()
         setIsOpen(false)
-      } else {
-        alert('전송 실패! 다시 시도해주세요.')
       }
     } catch (error) {
       console.error('전송 중 오류 발생:', error)
-      alert('전송 중 오류가 발생했습니다.')
+      toast.error('전달에 실패했어요...', { icon: '😭' })
     }
   }
 
   return createPortal(
     <div className="fixed inset-0 w-full max-w-[600px] h-screen bg-[url('/img/도치편지지.jpg')] bg-center bg-no-repeat bg-cover mx-auto ">
-      <div className="absolute inset-0 bg-white opacity-50 pointer-events-none"></div>
+      <div className="absolute inset-0 bg-white opacity-20 pointer-events-none"></div>
 
       <div className="my-6 mx-3">
         <div className="flex ">
@@ -51,13 +50,13 @@ export default function LetterPage({ setIsOpen }: LetterPageProps) {
           className="flex flex-col gap-6 mt-10">
           <input
             {...register('nickname')}
-            className="w-full p-2 border border-black/30 rounded-lg focus:bg-blue-100 focus:outline-none"
+            className="w-full p-2 border border-black/10 rounded-lg focus:bg-blue-100 focus:outline-none"
             placeholder="무슨 닉네임으로 보내고 싶나요?"
           />
 
           <textarea
             {...register('content')}
-            className="w-full h-60 p-3 border border-black/30 rounded-lg focus:bg-blue-100 focus:outline-none"
+            className="w-full h-60 p-3 border border-black/10 rounded-lg focus:bg-blue-100 focus:outline-none"
             placeholder="여기에 써주시면 돼요! 만약 이전에 많은 내용이 보내졌다면, 전달이 안될 수 있어요... "
           />
 
