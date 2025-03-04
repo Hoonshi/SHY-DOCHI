3e8098f9994026d68f54d154abb707d4d20c974a
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-03-04 19:46:34 +0900

    refactor: 스타일 수정

--- src/pages/introPage.tsx
+++ src/pages/introPage.tsx
@@ -7,7 +7,7 @@ export default function IntroPage() {
 
   return (
     <>
-      <div className=" p-5 relative max-w-s top-8 bg-[#EAEAEA] text-black rounded-full text-center leading-7">
+      <div className=" p-3 relative max-w-xs mx-auto top-8 bg-[#EAEAEA] text-black rounded-full text-xs text-center leading-4">
         안녕하세요~ 반가워요 🦔
         <br></br>익명으로 할 말이 있으시면, 제가 전해드릴게요~
         <br></br>답장은,,, 챙겨올 수 없지만,,, 보내드릴순 있어요!
@@ -16,7 +16,7 @@ export default function IntroPage() {
           onClick={() => setisOpen(true)}>
           여기 눌러주세요!
         </Button>
-        <div className="absolute bottom-[1px] left-[14rem] w-0 h-0 border-l-8 border-l-transparent border-t-15 border-t-[#EAEAEA] border-r-10 border-r-transparent transform translate-y-full"></div>
+        <div className="absolute bottom-[1px] left-[10rem] w-0 h-0 border-l-8 border-l-transparent border-t-15 border-t-[#EAEAEA] border-r-10 border-r-transparent transform translate-y-full"></div>
       </div>
       {isOpen && <LetterPage setIsOpen={setisOpen} />}
     </>
